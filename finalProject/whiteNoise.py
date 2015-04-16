74f397760db6ac0980014ccf631d009f980c5f73
bringsyrup <rubymspring@gmail.com>
2015-04-16 12:10:59 -0400

    added wav files

---
+++ finalProject/whiteNoise.py
@@ -0,0 +1,11 @@
+#! /bin/env python
+import thinkdsp as dsp
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+#recieve white noise with speaker. how?
+
+#load whitenoise.wav
+
+#do math with initial and recieved signal to get transfer function
