728c0fa39d187c0ebfc4416f96dddf6505ece755
bringsyrup <rubymspring@gmail.com>
2015-04-23 12:24:20 -0400

    added new wav files and bode plotting script

--- finalProject/whiteNoise.py
+++ finalProject/whiteNoise.py
@@ -2,10 +2,17 @@
 import thinkdsp as dsp
 import numpy as np
 import matplotlib.pyplot as plt
+import sys
+import bode
+
+def write(wavFiles, frameRate):
+    wavIn = thinkdsp.read_wave(wavFile[1])
+    wavOut = thinkdsp.read_wave(wavFile[2])
+    freqResp = len(wavIn) * np.correlate(wavIn.ys,wavOut.ys)
+    dsp.Wave(freqResp, frameRate).write('whiteNoiseFFT.wav')
+    bode.plot(freqResp, 'White noise')
+    return
 
 
-#recieve white noise with speaker. how?
-
-#load whitenoise.wav
-
-#do math with initial and recieved signal to get transfer function
+if __name__ == "__main__":
+    write(sys.argv[1:2], sys.argv[3])
